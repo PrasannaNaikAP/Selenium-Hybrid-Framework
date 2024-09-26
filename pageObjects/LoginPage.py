8d5be6f0fdcb5a0a0e0c89276ebaa4b4efce3b24
Prasanna Naik <74963249+PrasannaNaikAP@users.noreply.github.com>
2024-09-26 11:46:54 +0530

    Update LoginPage.py

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,1 +1,19 @@
+class LoginPage:
+  textbox_username_id = "Email"
+  textbox_password_id = "Password"
+  button_login_xpath = "//input[@value='Log in']"
+  link_logout_linktext = "Logout"
 
+  def __init__(self, driver):
+    self.driver=driver
+
+  def setUsername(self, username):
+    self.find_element(By.ID, textbox_username_id).clear()
+    self.find_element(By.ID, textbox_username_id).send_keys(username)
+
+  def setPassword(self, password):
+    self.find_element(By.ID, textbox_password_id).clear()
+    self.find_element(By.ID, textbox_password_id).send_keys(password)
+    
+    
+  
