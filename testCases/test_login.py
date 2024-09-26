fb2522bde736b92799ed6ae9d6525f585d40cdc5
Prasanna Naik <74963249+PrasannaNaikAP@users.noreply.github.com>
2024-09-26 13:35:48 +0530

    Update test_login.py

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,1 +1,24 @@
+import pytest
+from selenium import driver
+from pageObjects.LoginPage import LoginPage
 
+
+class Test_003_Login:
+  baseURL = ReadConfig.getApplicationURL()
+  username=ReadConfig.getUsername()
+
+  def Home_pageTitle(self, setup):
+    self.driver=setup
+    self.driver.get(self.baseURL)
+    title=driver.title
+    if title=="your store":
+      self.driver.close()
+      assert true
+
+  def LoginPage(self, setup):
+    self.driver=setup
+    self.driver.get(self.baseUrl)
+    self.logpage=LoginPage(self.driver)
+    self.logpage.setusername(self.username)
+    
+    
